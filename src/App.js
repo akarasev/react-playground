e196e37bf2cd392e620d54d0d3854e421eebb33e
Alex Karasev <alex.v.karasev@gmail.com>
2018-10-03 08:14:11 +0700

    Added dynamic styling and Radium.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   state = {
     persons: [
       { id: '01', name: "Max", age: 28 },
-      { id: '02', name: "Alex", age: 32 }
+      { id: '02', name: "Alex", age: 32 },
+      { id: '03', name: "Natali", age: 27 }
     ],
     showPersons: false
   }
@@ -40,7 +41,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px'
@@ -60,12 +62,25 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
-      )
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle persons</button>
